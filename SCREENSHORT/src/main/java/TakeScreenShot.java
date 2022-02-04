9b222ed6fe18e4aa32727c565fc8877cab037d7e
mekal <mekal@LAPTOP-56R4G7HS>
2022-02-04 20:31:29 +0530

    My first commit

---
+++ SCREENSHORT/src/main/java/TakeScreenShot.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class TakeScreenShot {
+ 
+ public static void screenShot(WebDriver driver, String filename) {
+  TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+  File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+  try {
+   FileUtils.copyFile(source, new File(System.getProperty("user.dir")+"\\ScreenShot\\"+filename+".png"));
+  } catch (Exception e) {
+   // TODO Auto-generated catch block
+   e.getMessage();
+  }
+ }
+}
---
+++ SCREENSHORT/src/test/java/ScreenShotTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ScreenShotTest {
+ WebDriver driver;
+
+ @BeforeMethod
+ public void setup() {
+  System.setProperty("webdriver.chrome.driver",
+    "C:\\study softwares\\chromedriver_win32\\chromedriver.exe");
+  driver = new ChromeDriver();
+  driver.manage().window().maximize();
+  driver.get("https://www.google.com/");
+ }
+ @Test
+ public void testCase1() throws InterruptedException {
+  driver.findElement(By.name("q")).sendKeys("Pawan Kalyan");
+  Thread.sleep(1000);
+  TakeScreenShot.screenShot(driver, "sh1");
+
+
+}
+
+ @AfterMethod
+ public void tearDown() {
+  driver.close();
+ }
+}
